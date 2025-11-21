e45bbe196db55e3b557f195deef4278ac6d6d0d4
genedeh <genesis.enedeh@gmail.com>
2025-11-21 18:06:27 +0100

    Key concepts

--- lib/main.dart
+++ lib/main.dart
@@ -40,11 +40,41 @@ class _MyHomePageState extends State<MyHomePage> {
       appBar: AppBar(
         backgroundColor: Theme.of(context).colorScheme.inversePrimary,
         title: Text(widget.title),
+        leading: Icon(Icons.notification_add),
       ),
-      body: Image.asset(
-        'assets/images/bg.jpg',
-        height: double.infinity,
-        fit: BoxFit.cover,
+      body: Column(
+        children: [
+          Container(
+            padding: EdgeInsets.all(25.0),
+            child: Stack(
+              alignment: AlignmentGeometry.center,
+              children: [
+                Image.asset(
+                  'assets/images/bg.jpg',
+                  fit: BoxFit.cover,
+                  height: 300,
+                  width: double.infinity,
+                ),
+                SizedBox(height: 300, child: Center(child: Text('Flutter'))),
+              ],
+            ),
+          ),
+          ListTile(
+            title: Text('Some Title'),
+            tileColor: Colors.red,
+            leading: Icon(Icons.join_full),
+            trailing: Text('Wow'),
+            onTap: () {
+              print('Click on Some title');
+            },
+          ),
+          Wrap(
+            children: [
+              Text('hhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhh'),
+              Text('hhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhh'),
+            ],
+          ),
+        ],
       ),
     );
   }
